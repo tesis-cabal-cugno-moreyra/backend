a62e3d0e4bde0768d1b2bdcad32eabd02025b4c8
Facundo Moreyra <fmoreyra2011@gmail.com>
2020-09-03 23:44:40 -0300

    hopefully, last config

--- sicoin/asgi.py
+++ sicoin/asgi.py
@@ -6,7 +6,12 @@ defined in the ASGI_APPLICATION setting.
 import os
 import django
 from channels.routing import get_default_application
+from configurations import importer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sicoin.config")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+
+importer.install()
+
 django.setup()
 application = get_default_application()
