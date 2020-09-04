abddb1e2fde66ea087e46b72c3bff796ee3db790
Facundo Moreyra <fmoreyra2011@gmail.com>
2020-09-03 22:12:27 -0300

    fix2

--- sicoin/asgi.py
+++ sicoin/asgi.py
@@ -7,7 +7,7 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sicoin.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sicoin.config")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
 django.setup()
 application = get_default_application()
