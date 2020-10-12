669f311f556618111b1c449d3bc4db0401b530bc
Facundo Moreyra <fmoreyra2011@gmail.com>
2020-10-11 22:16:58 -0300

    postGis setup

--- sicoin/config/common.py
+++ sicoin/config/common.py
@@ -28,6 +28,7 @@ class Common(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'django.contrib.gis',
 
 
         # Third party apps
